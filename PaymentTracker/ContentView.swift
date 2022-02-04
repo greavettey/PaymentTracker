223d906a2e27562b605388e37a5849f784c0c338
Axel Greavette <axelgreavette@gmail.com>
2022-02-04 14:54:33 -0500

    Initial Commit

---
+++ PaymentTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PaymentTracker
+//
+//  Created by Axel Greavette on 2022-02-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PaymentTracker/PaymentTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentTrackerApp.swift
+//  PaymentTracker
+//
+//  Created by Axel Greavette on 2022-02-04.
+//
+
+import SwiftUI
+
+@main
+struct PaymentTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
